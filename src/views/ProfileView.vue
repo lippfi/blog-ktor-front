52a38eaee342853400a9ce23761f0ecb5f806d89
filipp <x@lipp.fi>
2026-01-13 22:14:50 +0200

    Remove stub

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,56 +1,24 @@
 <script setup lang="ts">
 import ProcessedText from "@/components/post/ProcessedText.vue";
-import {profileStub} from "@/router";
 import Friends from "@/components/profile/Friends.vue";
 import Achievements from "@/components/profile/Achievements.vue";
 
 const props = defineProps<{
   login: string;
 }>();
-
-const text = `Привет, я детектив. Вот карточка с каким-то текстом и CSS, которая должна вас вдохновить создать что угодно свое.
-Просто знайте, что сюда можно вставить [b]любую[/b] html [i]css[/i] штуку, которую вы захотите.
-То есть тут может быть [g]что угодно[/g], просто я не умею делать красиво.
-[-]
-[c]Я cвято верю, что на сайте с дневниками
-Ограниченными должны быть пользователи
-А не возможности сайта, понимаете?[/c][/-]
-[r][classes=\"inverted\"]Хотите сделать стандартную беоновскую анкету? Пожалуйта
-Превратить это в предисловие? Ок
-Вставить пивного кота? Уважаю[/classes][/r]
-
-[link=\"https://beon.vip/user/21000/20822\"]ссылка на мой дневник[/link]
-[link=\"https://beon.vip/user/21000/20822\"]записи про удлиннение языка[/link]
-
-[classes=\"centered\"]
-[classes=\"columns card\"]
-  [classes=\"photo\"][image link=\"https://i.pinimg.com/550x/56/90/72/569072435a51a4c2690e08a3026de5a0.jpg\"][/classes]
-  [classes=\"columns\"]
-    [classes=\"fields\"][b]name:[/b]
-[b]age:[/b]
-[b]something:[/b]
-    [/classes]
-    [classes=\"answers\"]detective
-8 years old
-pupa
-    [/classes]
-  [/classes]
-[/classes]
-[/classes]
-`
 </script>
 
 <template>
   <div class="profile">
-    <ProcessedText :text="text" :avatars="avatars" @update-avatars="updateAvatars"/>/>
-    <div class="section friends-section">
-      <h2>Friends</h2>
-      <Friends :friends="profileStub.friends"/>
-    </div>
-    <div class="section achievements-section">
-      <h2>Achievements</h2>
-      <Achievements :achievements="profileStub.achievements"/>
-    </div>
+<!--    <ProcessedText :text="text" :avatars="avatars" @update-avatars="updateAvatars"/>/>-->
+<!--    <div class="section friends-section">-->
+<!--      <h2>Friends</h2>-->
+<!--      <Friends :friends="profileStub.friends"/>-->
+<!--    </div>-->
+<!--    <div class="section achievements-section">-->
+<!--      <h2>Achievements</h2>-->
+<!--      <Achievements :achievements="profileStub.achievements"/>-->
+<!--    </div>-->
   </div>
 </template>
 
