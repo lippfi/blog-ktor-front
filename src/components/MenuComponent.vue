ac029131a3a8bfca8652bb27fafd286ce9e50676
filipp <x@lipp.fi>
2026-04-04 01:58:26 +0300

    Better menu

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -12,7 +12,7 @@ import {
   User
 } from "@element-plus/icons-vue";
 import {useI18n} from "vue-i18n";
-import {useRouter} from 'vue-router';
+import {RouterLink, useRouter} from 'vue-router';
 import {getCurrentUserLogin, logOut} from "@/api/userClient.ts";
 import {computed, onMounted, ref} from 'vue';
 
@@ -68,47 +68,59 @@ onMounted(() => {
       v-if="!collapsed"
       class="el-menu-vertical"
   >
-    <el-menu-item index="1" @click="navigateTo('/')">
-      <el-icon><HomeFilled /></el-icon>
-      <span>{{ t('menu.home') }}</span>
+    <el-menu-item index="1">
+      <RouterLink to="/" class="menu-link">
+        <el-icon><HomeFilled /></el-icon>
+        <span>{{ t('menu.home') }}</span>
+      </RouterLink>
     </el-menu-item>
 
-    <el-menu-item index="2" @click="navigateTo(`/${currentUser}/profile`)">
-      <el-icon><User /></el-icon>
-      <span>{{ t('menu.profile') }}</span>
+    <el-menu-item index="2">
+      <RouterLink :to="`/${currentUser}/profile`" class="menu-link">
+        <el-icon><User /></el-icon>
+        <span>{{ t('menu.profile') }}</span>
+      </RouterLink>
     </el-menu-item>
 
-    <el-menu-item index="3" @click="navigateTo(`/${currentUser}/diary`)">
-      <el-icon><Memo /></el-icon>
-      <span>{{ t('menu.diary') }}</span>
+    <el-menu-item index="3">
+      <RouterLink :to="`/${currentUser}/diary`" class="menu-link">
+        <el-icon><Memo /></el-icon>
+        <span>{{ t('menu.diary') }}</span>
+      </RouterLink>
     </el-menu-item>
 
-    <el-menu-item index="4" @click="navigateTo('/messages')">
-      <el-icon><Message /></el-icon>
-      <span>{{ t('menu.messages') }}</span>
+    <!-- <el-menu-item index="4">
+      <RouterLink to="/messages" class="menu-link">
+        <el-icon><Message /></el-icon>
+        <span>{{ t('menu.messages') }}</span>
+      </RouterLink>
+    </el-menu-item> -->
+
+    <el-menu-item index="5">
+      <RouterLink to="/search" class="menu-link">
+        <el-icon><Search /></el-icon>
+        <span>{{ t('menu.search') }}</span>
+      </RouterLink>
     </el-menu-item>
 
-    <el-menu-item index="5" @click="navigateTo('/search')">
-      <el-icon><Search /></el-icon>
-      <span>{{ t('menu.search') }}</span>
+    <el-menu-item index="6">
+      <RouterLink to="/settings" class="menu-link">
+        <el-icon><Setting /></el-icon>
+        <span>{{ t('menu.settings') }}</span>
+      </RouterLink>
     </el-menu-item>
 
-    <el-menu-item index="6" @click="navigateTo('/settings')">
-      <el-icon><Setting /></el-icon>
-      <span>{{ t('menu.settings') }}</span>
-    </el-menu-item>
-
-    <el-menu-item index="7">
+    <!-- <el-menu-item index="7">
       <el-icon><Hide /></el-icon>
       <span>{{ t('menu.designOff') }}</span>
-    </el-menu-item>
+    </el-menu-item> -->
 
     <el-menu-item index="8" @click="signOut">
       <el-icon><SwitchButton /></el-icon>
       <span>{{ t('menu.logout') }}</span>
     </el-menu-item>
 
-    <div class="theme-toggle-item">
+    <!-- <div class="theme-toggle-item">
       <button
         class="theme-toggle-button"
         type="button"
@@ -121,7 +133,7 @@ onMounted(() => {
           <Moon v-else />
         </el-icon>
       </button>
-    </div>
+    </div> -->
   </el-menu>
 </template>
 
@@ -140,7 +152,19 @@ onMounted(() => {
   font-size: 16px;
   align-items: center;
   gap: 10px;
+  padding: 0;
   transition: all 0.15s ease;
+}
+
+.menu-link {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  color: inherit;
+  text-decoration: none;
 }
 
 .el-menu-item:hover {
